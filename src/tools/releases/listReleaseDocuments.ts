6a7933375c96b226d76d1bc88370d809f73db59a
Fredrik Mørstad <fredrik.morstad@hotmail.com>
2025-03-26 14:49:29 +0100

    add tool for listing releases

---
+++ src/tools/releases/listReleaseDocuments.ts
@@ -0,0 +1,52 @@
+import { SanityClient } from "@sanity/client";
+import { CallToolResult } from "@modelcontextprotocol/sdk/types.js";
+import { sanityClient } from "../../config/sanity.js";
+
+export async function listAllReleases(): Promise<CallToolResult> {
+  try {
+    let res = await listReleases(sanityClient);
+    if (!res) {
+      return {
+        content: [
+          {
+            type: "text",
+            text: `Could not find any releases`,
+          },
+        ],
+      };
+    }
+
+    let releases = JSON.stringify(res);
+
+    return {
+      content: [
+        {
+          type: "text",
+          text: releases,
+        },
+      ],
+    };
+  } catch (e: unknown) {
+    return {
+      content: [
+        {
+          type: "text",
+          text: `got error: ${e}`,
+        },
+      ],
+    };
+  }
+}
+export async function listReleases(
+  client: SanityClient,
+): Promise<Record<string, string>[]> {
+  const query = "releases::all()";
+  const params = {};
+
+  try {
+    let res = await client.fetch(query, params);
+    return res;
+  } catch (error: unknown) {
+    throw error;
+  }
+}
