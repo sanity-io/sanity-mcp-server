5f5941a40dadbf3fb2ff8c2af8354d33ff97ed5f
RostiMelk <hello@rosti.no>
2025-05-14 12:48:48 +0200

    feat: remove flag

--- src/tools/schema/common.ts
+++ src/tools/schema/common.ts
@@ -3,6 +3,6 @@ import {outdent} from 'outdent'
 export const SCHEMA_DEPLOYMENT_INSTRUCTIONS = outdent`
   Your Sanity schema has not been deployed. In your Sanity project, run the following command:
   \`\`\`shell
-  SANITY_CLI_SCHEMA_STORE_ENABLED=true npx sanity@latest schema deploy
+  npx sanity@latest schema deploy
   \`\`\`
 `
